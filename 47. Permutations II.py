b7aadbf594891eff89180a3905306ee596eee84d
zhangpeng <zhangpeng@gendock.com>
2017-06-17 16:49:44 +0800

    ADD: 47. Permutations II.py

--- 47. Permutations II.py
+++ 47. Permutations II.py
@@ -7,10 +7,24 @@ class Solution(object):
         import itertools
         result = set()
         for permut in itertools.permutations(nums, len(nums)):
-                result.add(permut)
+            result.add(permut)
 
         return [list(v) for v in result]
+
+    def permuteUnique2(self, nums):
+        ans = [[]]
+        for n in nums:
+            new_ans = []
+            for l in ans:
+                for i in xrange(len(l) + 1):
+                    new_ans.append(l[:i] + [n] + l[i:])
+                    # if i < len(l) and l[i] == n:
+                    if i < len(l) and l[i] == n: #index check is for forbidden out of index exception
+                        break  # handles duplication
+            ans = new_ans
+        return ans
 
 
 if __name__ == '__main__':
     print Solution().permuteUnique([1, 1, 2])
+    print Solution().permuteUnique2([1, 1, 2])
