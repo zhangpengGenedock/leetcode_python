7b4d025433eb50cef7ef72c425a51a97022dac28
zhangpeng <zhangpeng@fenbi.com>
2017-06-20 22:58:57 +0800

    ADD: 68. Text Justification.py

---
+++ 68. Text Justification.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def fullJustify(self, words, maxWidth):
+        """
+        :type words: List[str]
+        :type maxWidth: int
+        :rtype: List[str]
+        """
+        res, cur, num_of_letters = [], [], 0
+        for w in words:
+            if num_of_letters + len(cur) + len(w) > maxWidth:
+                for i in range(maxWidth - num_of_letters):
+                    cur[i % (len(cur) - 1 or 1)] += ' '
+                res.append(''.join(cur))
+                cur, num_of_letters = [], 0
+            cur += [w]
+            num_of_letters += len(w)
+        return res + [' '.join(cur).ljust(maxWidth)]
+
+
+if __name__ == '__main__':
+    for line in Solution().fullJustify(["This", "is", "an", "example", "of", "text", "justification."], 16):
+        print line
