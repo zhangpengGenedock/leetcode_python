10355cece911f3f5d102273cf20ca08d223e0c3c
zhangpeng <zhangpeng@fenbi.com>
2017-07-04 22:57:18 +0800

    ADD: 99. Recover Binary Search Tree.py

---
+++ 99. Recover Binary Search Tree.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+def inorder_iter(root):
+    if root:
+        for node in inorder_iter(root.left):
+            yield node
+        yield root
+        for node in inorder_iter(root.right):
+            yield node
+
+
+class Solution(object):
+    def recoverTree(self, root):
+        """
+        https://discuss.leetcode.com/topic/5326/elegant-python-code-using-yield-iterator
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+        """
+        pred, first = None, None
+        for node in inorder_iter(root):
+            if pred and pred.val > node.val:
+                if first is None:
+                    first = pred
+                    second = node
+                else:
+                    second = node
+                    break
+            pred = node
+        first.val, second.val = second.val, first.val
