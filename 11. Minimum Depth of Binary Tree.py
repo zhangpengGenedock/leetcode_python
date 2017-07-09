76bca443e4e4578447a5f5520688ea90bd5d8cf3
zhangpeng <zhangpeng@gendock.com>
2017-07-09 19:56:22 +0800

    ADD: 11. Minimum Depth of Binary Tree.py

---
+++ 11. Minimum Depth of Binary Tree.py
@@ -0,0 +1,19 @@
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def minDepth(self, root):
+        """
+        https://discuss.leetcode.com/topic/3339/my-solution-in-python
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
+        if not root.left or not root.right:
+            return self.minDepth(root.left) + self.minDepth(root.right) + 1
+        return min(self.minDepth(root.left), self.minDepth(root.right)) + 1
