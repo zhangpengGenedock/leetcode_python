c8aa93124ddca35fb2e49c01cb8576257a96bd69
zhangpeng <zhangpeng@fenbi.com>
2017-07-25 22:30:57 +0800

    ADD: 132. Palindrome Partitioning II.py

---
+++ 132. Palindrome Partitioning II.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def minCut(self, s):
+        """
+        https://discuss.leetcode.com/topic/22388/56-ms-python-with-explanation/2
+        :type s: str
+        :rtype: int
+        """
+        if s == s[::-1]: return 0
+        for i in range(1, len(s)):
+            if s[:i] == s[:i][::-1] and s[i:] == s[i:][::-1]:
+                return 1
+        #add above check, can improve performance very much
+        cut = [x for x in range(-1, len(s))]
+        for i in range(0, len(s)):
+            for j in range(i, len(s)):
+                if s[i:j + 1] == s[i:j + 1][::-1]:
+                    cut[j + 1] = min(cut[j + 1], cut[i] + 1)
+        return cut[-1]
+
+
+if __name__ == '__main__':
+    print Solution().minCut('aab')
