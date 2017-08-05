11ff100319e7ec3de2198e749902a3047083b304
zhangpeng <zhangpeng@gendock.com>
2017-08-05 15:46:50 +0800

    ADD: review/21. Merge Two Sorted Lists.py

---
+++ review/21. Merge Two Sorted Lists.py
@@ -0,0 +1,36 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        https://discuss.leetcode.com/topic/21292/python-solutions-iteratively-recursively-iteratively-in-place
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        dummy = cur = ListNode(0)
+        while l1 and l2:
+            if l1.val < l2.val:
+                cur.next = l1
+                l1 = l1.next
+            else:
+                cur.next = l2
+                l2 = l2.next
+            cur = cur.next
+        cur.next = l1 or l2
+        return dummy.next
+
+    def mergeTwoLists2(self, l1, l2):
+        if not l1 or not l2:
+            return l1 or l2
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
