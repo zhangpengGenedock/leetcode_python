e38e09cdac2c72376ae53f660f0538f75cce7b70
zhangpeng <zhangpeng@gendock.com>
2017-08-05 17:45:26 +0800

    ADD: review/24. Swap Nodes in Pairs.py

---
+++ review/24. Swap Nodes in Pairs.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def swapPairs(self, head):
+        """
+        https://discuss.leetcode.com/topic/18860/7-8-lines-c-python-ruby
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        pre, pre.next = self, head
+        while pre.next and pre.next.next:
+            a = pre.next
+            b = a.next
+            pre.next, b.next, a.next = b, a, b.next
+            pre = a
+        return self.next
