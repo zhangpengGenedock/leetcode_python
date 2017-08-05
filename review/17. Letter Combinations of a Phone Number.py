27bd44ce54008e311b2703834a7d1b777e2806ec
zhangpeng <zhangpeng@gendock.com>
2017-08-05 12:24:47 +0800

    ADD: review/16. 3Sum Closest.py and review/17. Letter Combinations of a Phone Number.py

---
+++ review/17. Letter Combinations of a Phone Number.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        """
+        https://discuss.leetcode.com/topic/11783/one-line-python-solution
+        :type digits: str
+        :rtype: List[str]
+        """
+        if '' == digits:
+            return []
+        kvmaps = {
+            '2': 'abc',
+            '3': 'def',
+            '4': 'ghi',
+            '5': 'jkl',
+            '6': 'mno',
+            '7': 'pqrs',
+            '8': 'tuv',
+            '9': 'wxyz'
+        }
+        return reduce(lambda acc, digit: [x + y for x in acc for y in kvmaps[digit]], digits, [''])
