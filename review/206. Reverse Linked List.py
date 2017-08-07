c63cb5c103d5f5dd41443c7e9828028bd69702af
zhangpeng <zhangpeng@fenbi.com>
2017-08-07 22:08:25 +0800

    ADD: review/206. Reverse Linked List.py

---
+++ review/206. Reverse Linked List.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        pred = None
+        while head:
+            curr = head
+            head = head.next
+            curr.next = pred
+            pred = curr
+        return pred
+
+    def reverseList2(self, head):
+        return self._reverse(head)
+
+    def _reverse(self, node, pred=None):
+        if not node:
+            return pred
+        n = node.next
+        node.next = pred
+        return self._reverse(n, node)
