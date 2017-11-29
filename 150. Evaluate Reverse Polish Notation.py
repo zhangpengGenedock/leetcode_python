3bc392d840dd125047c43be3588430d74d77c850
zhangpeng <zhangpeng@gendock.com>
2017-11-29 11:47:37 +0800

    ADD: 150. Evaluate Reverse Polish Notation.py

---
+++ 150. Evaluate Reverse Polish Notation.py
@@ -0,0 +1,33 @@
+# Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+#
+# Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+#
+# Some examples:
+#   ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+#   ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+#
+
+class Solution(object):
+    def evalRPN(self, tokens):
+        """
+        :type tokens: List[str]
+        :rtype: int
+        """
+        stack = []
+        for t in tokens:
+            if t not in ['+', '-', '*', '/']:
+                stack.append(int(t))
+            else:
+                r, l = stack.pop(), stack.pop()
+                if t == '+':
+                    stack.append(l + r)
+                elif t == '-':
+                    stack.append(l - r)
+                elif t == '*':
+                    stack.append(l * r)
+                else:
+                    if l * r < 0 and l % r != 0:
+                        stack.append(l / r + 1)
+                    else:
+                        stack.append(l / r)
+        return stack.pop()
