580d202185e5c413ff730a18f09f7fd452942fc9
zhangpeng <zhangpeng@gendock.com>
2017-12-01 11:03:47 +0800

    ADD: 155. Min Stack.py

---
+++ 155. Min Stack.py
@@ -0,0 +1,84 @@
+# Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+#
+# push(x) -- Push element x onto stack.
+# pop() -- Removes the element on top of the stack.
+# top() -- Get the top element.
+# getMin() -- Retrieve the minimum element in the stack.
+
+class MinStack(object):
+    """
+    https://discuss.leetcode.com/topic/11985/my-python-solution/2
+    """
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.q = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        curMin = self.getMin()
+        if curMin == None or x < curMin:
+            curMin = x
+        self.q.append((x, curMin))
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.q.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if self.q:
+            return self.q[-1][0]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if self.q:
+            return self.q[-1][1]
+
+
+class MinStack2(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.minStack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self.stack.append(x)
+        if not self.minStack or x <= self.minStack[-1]:
+            self.minStack.append(x)
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        if self.minStack and self.minStack[-1] == self.stack.pop():
+            self.minStack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.minStack[-1]
