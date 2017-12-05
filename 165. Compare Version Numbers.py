b39d2a526b556a66361f44aa2809099ba8fc1355
zhangpeng <zhangpeng@gendock.com>
2017-12-05 18:54:42 +0800

    ADD: 165. Compare Version Numbers.py

---
+++ 165. Compare Version Numbers.py
@@ -0,0 +1,32 @@
+# Compare two version numbers version1 and version2.
+# If version1 > version2 return 1, if version1 < version2 return -1, otherwise return 0.
+#
+# You may assume that the version strings are non-empty and contain only digits and the . character.
+# The . character does not represent a decimal point and is used to separate number sequences.
+# For instance, 2.5 is not "two and a half" or "half way to version three", it is the fifth second-level revision of the second first-level revision.
+#
+# Here is an example of version numbers ordering:
+#
+# 0.1 < 1.1 < 1.2 < 13.37
+# Credits:
+# Special thanks to @ts for adding this problem and creating all test cases.
+
+
+class Solution:
+    def compareVersion(self, version1, version2):
+        """
+        https://discuss.leetcode.com/topic/28438/python-10-lines-solution
+        :type version1: str
+        :type version2: str
+        :rtype: int
+        """
+        versions1 = [int(v) for v in version1.split('.')]
+        versions2 = [int(v) for v in version2.split('.')]
+        for i in range(max(len(versions1), len(versions2))):
+            v1 = versions1[i] if i < len(versions1) else 0
+            v2 = versions2[i] if i < len(versions2) else 0
+            if v1 > v2:
+                return 1
+            elif v1 < v2:
+                return -1
+        return 0
