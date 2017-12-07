910a04cd1d0fa355ccab5fdcdb87d7221c55794c
zhangpeng <zhangpeng@gendock.com>
2017-12-07 18:52:19 +0800

    ADD: 162. Find Peak Element.py

---
+++ 162. Find Peak Element.py
@@ -0,0 +1,33 @@
+# A peak element is an element that is greater than its neighbors.
+#
+# Given an input array where num[i] ≠ num[i+1], find a peak element and return its index.
+#
+# The array may contain multiple peaks, in that case return the index to any one of the peaks is fine.
+#
+# You may imagine that num[-1] = num[n] = -∞.
+#
+# For example, in array [1, 2, 3, 1], 3 is a peak element and your function should return the index number 2.
+#
+# click to show spoilers.
+#
+# Credits:
+# Special thanks to @ts for adding this problem and creating all test cases.
+
+
+class Solution:
+    def findPeakElement(self, nums):
+        """
+        solution 思路： https://leetcode.com/problems/find-peak-element/solution/
+        python3
+        
+        :type nums: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = (l + r) // 2
+            if nums[mid] > nums[mid + 1]:
+                r = mid
+            else:
+                l = mid + 1
+        return l
