8d510393af6e90f7af120ff5c8efc1f59335746c
zhangpeng <zhangpeng@gendock.com>
2018-01-18 18:28:45 +0800

    ADD: 214. Shortest Palindrome.py question

---
+++ 214. Shortest Palindrome.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+
+__author__ = 'zhangpeng'
+
+"""Given a string S, you are allowed to convert it to a palindrome by adding characters in front of it. Find and 
+return the shortest palindrome you can find by performing this transformation. 
+
+For example:
+
+Given "aacecaaa", return "aaacecaaa".
+
+Given "abcd", return "dcbabcd".
+
+
+"""
+
+
+class Solution(object):
+    def shortestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+
