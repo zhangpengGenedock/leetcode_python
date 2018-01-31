1c2d02955c03b5726dd6a6b950a895d21e1b80e4
zhangpeng <zhangpeng@gendock.com>
2018-01-31 17:48:33 +0800

    ADD: 329. Longest Increasing Path in a Matrix.py

---
+++ 329. Longest Increasing Path in a Matrix.py
@@ -0,0 +1,56 @@
+# -*- coding:utf-8 -*-
+
+__author__ = 'zhangpeng'
+
+"""
+Given an integer matrix, find the length of the longest increasing path.
+
+From each cell, you can either move to four directions: left, right, up or down. You may NOT move diagonally or move 
+outside of the boundary (i.e. wrap-around is not allowed). 
+
+Example 1:
+
+nums = [
+  [9,9,4],
+  [6,6,8],
+  [2,1,1]
+]
+Return 4
+The longest increasing path is [1, 2, 6, 9].
+
+Example 2:
+
+nums = [
+  [3,4,5],
+  [3,2,6],
+  [2,2,1]
+]
+Return 4
+The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
+"""
+
+
+class Solution(object):
+    def longestIncreasingPath(self, matrix):
+        """
+        personal solution: 
+        :type matrix: List[List[int]]
+        :rtype: int
+        """
+
+        def dfs(i, j):
+            if not dp[i][j]:
+                val = matrix[i][j]
+                dp[i][j] = 1 + max(
+                    dfs(i - 1, j) if i and val < matrix[i - 1][j] else 0,
+                    dfs(i + 1, j) if i < M - 1 and val < matrix[i + 1][j] else 0,
+                    dfs(i, j + 1) if j < N - 1 and val < matrix[i][j + 1] else 0,
+                    dfs(i, j - 1) if j and val < matrix[i][j - 1] else 0
+                )
+            return dp[i][j]
+
+        if not matrix or not matrix[0]:
+            return 0
+        M, N = len(matrix), len(matrix[0])
+        dp = [[0] * N for _ in range(M)]
+        return max(dfs(x, y) for x in range(M) for y in range(N))
