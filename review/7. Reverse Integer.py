5a4a00f94142cd2c9e0b296ac9a3d0805a653de2
zhangpeng <zhangpeng@gendock.com>
2018-03-06 16:56:37 +0800

    ADD: 7. Reverse Integer.py new answer

--- review/7. Reverse Integer.py
+++ review/7. Reverse Integer.py
@@ -29,3 +29,19 @@ class Solution(object):
         s = cmp(x, 0)
         r = int(s * x[::-1])
         return s * r * (r < 2 ** 31)
+
+    def revers2(self, x):
+        """
+        If overflow exists, the new result will not equal previous one.
+        No flags needed. No hard code like 0xf7777777 needed.
+        :param x: 
+        :return: 
+        """
+        result = 0
+        while x != 0:
+            tail = x % 10
+            new_result = result * 10 + tail
+            if (new_result - tail) / 10 != result:
+                return 0
+            result = new_result
+            x = x / 10
