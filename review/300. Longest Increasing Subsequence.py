68569ca7b33c3d7e512aeebc6d5227d385fa113b
zhangpeng <zhangpeng@gendock.com>
2018-03-07 11:17:25 +0800

    ADD: explanations of 300. Longest Increasing Subsequence.py

--- review/300. Longest Increasing Subsequence.py
+++ review/300. Longest Increasing Subsequence.py
@@ -15,6 +15,19 @@ class Solution(object):
     def lengthOfLIS(self, nums):
         """
         https://discuss.leetcode.com/topic/28738/java-python-binary-search-o-nlogn-time-with-explanation/2
+        
+        tails is an array storing the smallest tail of all increasing subsequences with length i+1 in tails[i].
+        For example, say we have nums = [4,5,6,3], then all the available increasing subsequences are:
+
+        len = 1   :      [4], [5], [6], [3]   => tails[0] = 3
+        len = 2   :      [4, 5], [5, 6]       => tails[1] = 5
+        len = 3   :      [4, 5, 6]            => tails[2] = 6
+        We can easily prove that tails is a increasing array. Therefore it is possible to do a binary search in tails array to find the one needs update.
+
+        Each time we only do one of the two:
+
+        (1) if x is larger than all tails, append it, increase the size by 1
+        (2) if tails[i-1] < x <= tails[i], update tails[i]
         :type nums: List[int]
         :rtype: int
         """
