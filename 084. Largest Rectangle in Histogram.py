b9cb8a467ab980ca492a6bf58a3056103c9838ca
zhangpeng <zhangpeng@gendock.com>
2018-03-08 17:31:39 +0800

    ADD: explanation of 084. Largest Rectangle in Histogram.py

--- 084. Largest Rectangle in Histogram.py
+++ 084. Largest Rectangle in Histogram.py
@@ -6,8 +6,12 @@ Given n non-negative integers representing the histogram's bar height where the 
 
 Above is a histogram where width of each bar is 1, given height = [2,1,5,6,2,3].
 
+{@link https://leetcode.com/static/images/problemset/histogram.png}
+
 
 The largest rectangle is shown in the shaded area, which has area = 10 unit.
+
+https://leetcode.com/static/images/problemset/histogram_area.png
 
 For example,
 Given heights = [2,1,5,6,2,3],
@@ -17,7 +21,14 @@ class Solution(object):
     def largestRectangleArea(self, height):
         """
         https://discuss.leetcode.com/topic/27840/ac-python-clean-solution-using-stack-76ms
-        :param height:
+        
+        The stack maintain the indexes of buildings with ascending height. Before adding a new building pop the 
+        building who is taller than the new one. The building popped out represent the height of a rectangle with the 
+        new building as the right boundary and the current stack top as the left boundary. Calculate its area and 
+        update ans of maximum area. Boundary is handled using dummy buildings. 
+        
+        
+        :param height: 
         """
         height.append(0)
         stack = [-1]
