97d6d6dd8d0e9193cd4d279f4c1bee3ddc8cba24
zhangpeng <zhangpeng@gendock.com>
2018-03-08 17:10:27 +0800

    MOD: Search in Rotated Sorted Array II.py description

--- 081. Search in Rotated Sorted Array II.py
+++ 081. Search in Rotated Sorted Array II.py
@@ -25,12 +25,23 @@ class Solution(object):
         
         The idea is the same as the previous one without duplicates
 
-        1) everytime check if targe == nums[mid], if so, we find it.
-        2) otherwise, we check if the first half is in order (i.e. nums[left]<=nums[mid]) 
-          and if so, go to step 3), otherwise, the second half is in order,   go to step 4)
-        3) check if target in the range of [left, mid-1] (i.e. nums[left]<=target < nums[mid]), if so, do search in the first half, i.e. right = mid-1; otherwise, search in the second half left = mid+1;
-        4)  check if target in the range of [mid+1, right] (i.e. nums[mid]<target <= nums[right]), if so, do search in the second half, i.e. left = mid+1; otherwise search in the first half right = mid-1;
-        The only difference is that due to the existence of duplicates, we can have nums[left] == nums[mid] and in that case, the first half could be out of order (i.e. NOT in the ascending order, e.g. [3 1 2 3 3 3 3]) and we have to deal this case separately. In that case, it is guaranteed that nums[right] also equals to nums[mid], so what we can do is to check if nums[mid]== nums[left] == nums[right] before the original logic, and if so, we can move left and right both towards the middle by 1. and repeat.
+        1) everytime check if targe == nums[mid], if so, we find it. 
+        
+        2) otherwise, we check if the first half is in order (i.e. nums[left]<=nums[mid]) and if so, go to step 3), 
+        otherwise, the second half is in order,   go to step 4) 
+        
+        3) check if target in the range of [left, mid-1] (i.e. nums[left]<=target < nums[mid]), if so, do search in 
+        the first half, i.e. right = mid-1; otherwise, search in the second half left = mid+1; 
+        
+        4)  check if target in the range of [mid+1, right] (i.e. 
+        nums[mid]<target <= nums[right]), if so, do search in the second half, i.e. left = mid+1; otherwise search in 
+        the first half right = mid-1; 
+        
+        The only difference is that due to the existence of duplicates, we can have nums[left] == nums[mid] and in 
+        that case, the first half could be out of order (i.e. NOT in the ascending order, e.g. [3 1 2 3 3 3 3]) and 
+        we have to deal this case separately. In that case, it is guaranteed that nums[right] also equals to nums[
+        mid], so what we can do is to check if nums[mid]== nums[left] == nums[right] before the original logic, 
+        and if so, we can move left and right both towards the middle by 1. and repeat. 
 
         """
         left, right = 0, len(nums) - 1
