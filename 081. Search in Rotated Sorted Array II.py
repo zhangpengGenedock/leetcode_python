a59bf5036443f3596626680f78802a962cf837ce
zhangpeng <zhangpeng@gendock.com>
2018-03-08 16:55:55 +0800

    MOD: modify Search in Rotated Sorted Array II.py solution

--- 081. Search in Rotated Sorted Array II.py
+++ 081. Search in Rotated Sorted Array II.py
@@ -14,27 +14,43 @@ Write a function to determine if a given target is in the array.
 
 The array may contain duplicates.
 """
+
+
 class Solution(object):
     def search(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: bool
+        
+        The idea is the same as the previous one without duplicates
+
+        1) everytime check if targe == nums[mid], if so, we find it.
+        2) otherwise, we check if the first half is in order (i.e. nums[left]<=nums[mid]) 
+          and if so, go to step 3), otherwise, the second half is in order,   go to step 4)
+        3) check if target in the range of [left, mid-1] (i.e. nums[left]<=target < nums[mid]), if so, do search in the first half, i.e. right = mid-1; otherwise, search in the second half left = mid+1;
+        4)  check if target in the range of [mid+1, right] (i.e. nums[mid]<target <= nums[right]), if so, do search in the second half, i.e. left = mid+1; otherwise search in the first half right = mid-1;
+        The only difference is that due to the existence of duplicates, we can have nums[left] == nums[mid] and in that case, the first half could be out of order (i.e. NOT in the ascending order, e.g. [3 1 2 3 3 3 3]) and we have to deal this case separately. In that case, it is guaranteed that nums[right] also equals to nums[mid], so what we can do is to check if nums[mid]== nums[left] == nums[right] before the original logic, and if so, we can move left and right both towards the middle by 1. and repeat.
+
         """
-        if len(nums) == 0: return False
-        start, end = 0, len(nums) - 1
-        for i in range(len(nums) - 1):
-            if nums[i] > nums[i + 1]:
-                start, end = i + 1, i + len(nums)
-        while start <= end:
-            mid = start + (end - start) / 2
-            cur = nums[mid % len(nums)]
-            if cur == target:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) >> 1
+            if nums[mid] == target:
                 return True
-            elif cur > target:
-                end = mid - 1
+            if nums[left] == nums[mid] and nums[right] == nums[mid]:
+                left += 1
+                right -= 1
+            elif nums[left] <= nums[mid]:
+                if nums[left] <= target and nums[mid] > target:
+                    right = mid - 1
+                else:
+                    left = mid + 1
             else:
-                start = mid + 1
+                if nums[mid] < target and nums[right] >= target:
+                    left = mid + 1
+                else:
+                    right = mid - 1
         return False
 
 
