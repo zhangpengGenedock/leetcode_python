75203825f703960887c278ed68057615d9d12d6a
zhangpeng <zhangpeng@gendock.com>
2018-03-21 11:40:45 +0800

    ADD: questions descriptions for 129-142

--- 135. Candy.py
+++ 135. Candy.py
@@ -1,3 +1,14 @@
+"""
+https://leetcode.com/problems/candy/description/
+
+There are N children standing in a line. Each child is assigned a rating value.
+
+You are giving candies to these children subjected to the following requirements:
+
+Each child must have at least one candy.
+Children with a higher rating get more candies than their neighbors.
+What is the minimum candies you must give?
+"""
 class Solution(object):
     def candy(self, ratings):
         """
--- 138. Copy List with Random Pointer.py
+++ 138. Copy List with Random Pointer.py
@@ -1,3 +1,10 @@
+"""
+https://leetcode.com/problems/copy-list-with-random-pointer/description/
+
+A linked list is given such that each node contains an additional random pointer which could point to any node in the list or null.
+
+Return a deep copy of the list.
+"""
 # Definition for singly-linked list with a random pointer.
 class RandomListNode(object):
     def __init__(self, x):
