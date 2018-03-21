e4ba4b774abf06255e71363e0aeb62a909963742
zhangpeng <zhangpeng@gendock.com>
2018-03-21 14:48:03 +0800

    ADD: explanations for 142. Linked List Cycle II.py

--- 142. Linked List Cycle II.py
+++ 142. Linked List Cycle II.py
@@ -18,7 +18,24 @@ class ListNode(object):
 class Solution(object):
     def detectCycle(self, head):
         """
-        https://discuss.leetcode.com/topic/17521/share-my-python-solution-with-detailed-explanation`
+        https://discuss.leetcode.com/topic/17521/share-my-python-solution-with-detailed-explanation
+        
+        Consider the following linked list, where E is the cylce entry and X, the crossing point of fast and slow.
+        H: distance from head to cycle entry E
+        D: distance from E to X
+        L: cycle length
+                          _____
+                         /     \
+        head_____H______E       \
+                        \       /
+                         X_____/   
+        
+    
+        If fast and slow both start at head, when fast catches slow, slow has traveled H+D and fast 2(H+D). 
+        Assume fast has traveled n loops in the cycle, we have:
+        2H + 2D = H + D + L  -->  H + D = nL  --> H = nL - D
+        Thus if two pointers start from head and X, respectively, one first reaches E, the other also reaches E. 
+        In my solution, since fast starts at head.next, we need to move slow one step forward in the beginning of part 2
         :type head: ListNode
         :rtype: ListNode
         """
