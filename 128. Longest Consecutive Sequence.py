63d3e14d46c76b7e377fdbff8235c0c967b649dd
zhangpeng <zhangpeng@gendock.com>
2018-03-21 11:34:15 +0800

    MOD: solution for 128. Longest Consecutive Sequence.py

--- 128. Longest Consecutive Sequence.py
+++ 128. Longest Consecutive Sequence.py
@@ -10,26 +10,6 @@ The longest consecutive elements sequence is [1, 2, 3, 4]. Return its length: 4.
 Your algorithm should run in O(n) complexity.
 """
 class Solution(object):
-    def longestConsecutive(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        numbers = set(nums)
-        max_len = 0
-        while numbers:
-            m = n = numbers.pop()
-            length = 1
-            while m - 1 in numbers:
-                numbers.remove(m - 1)
-                m -= 1
-                length += 1
-            while n + 1 in numbers:
-                numbers.remove(n + 1)
-                n += 1
-                length += 1
-            max_len = max(max_len, length)
-        return max_len
 
     def longestConsecutive2(self, nums):
         """
